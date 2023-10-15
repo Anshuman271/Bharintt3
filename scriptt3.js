fa8c8b6b2552a86f9432d2f8499c28bd21e8d57a
Anshuman271 <145823800+Anshuman271@users.noreply.github.com>
2023-10-15 12:34:11 +0530

    Create scriptt3.js

---
+++ scriptt3.js
@@ -0,0 +1,4 @@
+// Adding a simple click event for the "Subscribe" button
+document.getElementById('subscribe-button').addEventListener('click', function () {
+    alert('Thank you for subscribing to Netflix!');
+});
